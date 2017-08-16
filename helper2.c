f27fee6b2731afa18d38eb3f50c56a1d2df43e8d
Larry Madeo <lmadeo76@gmail.com>
2017-08-16 06:39:39 +0000

    new delegate_op function skeleton

---
+++ helper2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * delegate_op - based on tokens from single line, decide what function to call
+ * @line_number: line in byte-code file being processed, zero indexed
+ *    Note: global glob is out there for use!
+ * Return: int for mysterious purposes
+ **/
+int delegate_op(unsigned int line_number)
+{
+	return (EXIT_SUCCESS)
+}
